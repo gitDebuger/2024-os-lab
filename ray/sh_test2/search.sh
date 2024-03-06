fd6abb59b3c5c19f7469049c486b2dba90e25ffa
HuYunge <silver_wolf@hyg>
2024-03-06 20:24:35 +0800

    ray committed the first time

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+file=$1
+str=$2
+result=$3
+
+grep -n $str $file | awk -F: '{print $1}' > $result
