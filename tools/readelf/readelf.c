f9bdecb756306e9df73375c5abf0e68c93ba3ad7
胡纭阁 <22371264@buaa.edu.cn>
2024-03-14 12:50:19 +0800

    update readelf.c

--- tools/readelf/readelf.c
+++ tools/readelf/readelf.c
@@ -43,7 +43,7 @@ int readelf(const void *binary, size_t size) {
 	Elf32_Half sh_entry_count;
 	Elf32_Half sh_entry_size;
 	/* Exercise 1.1: Your code here. (1/2) */
-	sh_table = (void *)((Elf32_Addr)binary + ehdr->e_shoff);
+	sh_table = (const void *)((uintptr_t)binary + ehdr->e_shoff);
 	sh_entry_count = ehdr->e_shnum;
 	sh_entry_size = ehdr->e_shentsize;
 
@@ -53,7 +53,7 @@ int readelf(const void *binary, size_t size) {
 		const Elf32_Shdr *shdr;
 		unsigned int addr;
 		/* Exercise 1.1: Your code here. (2/2) */
-		shdr = ((Elf32_Shdr *)sh_table + i);
+		shdr = (const Elf32_Shdr *)((uintptr_t)sh_table + i * sh_entry_size);
 		addr = shdr->sh_addr;
 
 		printf("%d:0x%x\n", i, addr);
