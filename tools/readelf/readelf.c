f27454fd33e6a63a9079ef3a8aa44e23f6d1fbd3
HuYunge <silver_wolf@hyg>
2024-03-14 11:57:45 +0800

    update readelf.c

--- tools/readelf/readelf.c
+++ tools/readelf/readelf.c
@@ -55,7 +55,7 @@ int readelf(const void *binary, size_t size) {
 		/* Exercise 1.1: Your code here. (2/2) */
 		shdr = (Elf32_Shdr *)sh_table;
 		addr = shdr->sh_addr;
-		sh_table = (void *)((uint32_t)sh_table + sh_entry_size + shdr->sh_size);
+		sh_table = (void *)((uint32_t)sh_table + shdr->sh_offset);
 
 		printf("%d:0x%x\n", i, addr);
 	}
