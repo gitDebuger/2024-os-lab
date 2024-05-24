a638b3527848ceb255bbc457c18e51a06df8245a
HuYunge <silver_wolf@hyg>
2024-05-24 08:47:12 +0800

    commit lab6

--- user/sh.c
+++ user/sh.c
@@ -91,8 +91,11 @@ int parsecmd(char **argv, int *rightpipe) {
 			// utilize 'debugf' to print relevant messages,
 			// and subsequently terminate the process using 'exit'.
 			/* Exercise 6.5: Your code here. (1/3) */
+			fd = open(t, O_RDONLY);
+			dup(fd, 0);
+			close(fd);
 
-			user_panic("< redirection not implemented");
+			// user_panic("< redirection not implemented");
 
 			break;
 		case '>':
@@ -106,8 +109,11 @@ int parsecmd(char **argv, int *rightpipe) {
 			// utilize 'debugf' to print relevant messages,
 			// and subsequently terminate the process using 'exit'.
 			/* Exercise 6.5: Your code here. (2/3) */
+			fd = open(t, O_WRONLY);
+			dup(fd, 1);
+			close(fd);
 
-			user_panic("> redirection not implemented");
+			// user_panic("> redirection not implemented");
 
 			break;
 		case '|':;
@@ -128,8 +134,21 @@ int parsecmd(char **argv, int *rightpipe) {
 			 */
 			int p[2];
 			/* Exercise 6.5: Your code here. (3/3) */
+			pipe(p);
+			*rightpipe = fork();
+			if (*rightpipe == 0) {
+				dup(p[0], 0);
+				close(p[0]);
+				close(p[1]);
+				return parsecmd(argv, rightpipe);
+			}  else if (*rightpipe > 0) {
+				dup(p[1], 1);
+				close(p[1]);
+				close(p[0]);
+				return argc;
+			}
 
-			user_panic("| not implemented");
+			// user_panic("| not implemented");
 
 			break;
 		}
